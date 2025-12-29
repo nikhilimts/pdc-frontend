564d21f401feacb3e35ca8917f9719b2bd72f88d
Siddhantji <teotiasiddhant@gmail.com>
2025-12-29 15:41:31 +0530

    Fix asset paths in index.html and update package-lock.json for peer dependencies; adjust Vite config for subfolder deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
-  // base: '/ERP/',
-
+  base: './',  // << important for subfolder deployment
   plugins: [react()],
 });
-
-// export default defineConfig({
-//   plugins: [react({
-//     include: "**/*.jsx",
-//   })]
-// })
